efa5fdbef56a80d386d31d8ad64865f306b93e8b
loki <gunlinux@ya.ru>
2024-01-24 04:26:20 +1000

    basic nvim

---
+++ nvim/lua/options.lua
@@ -0,0 +1,30 @@
+vim.opt.hidden= true
+vim.opt.showcmd = true
+vim.opt.nu = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+--vim.opt.backspace=indent,eol,start
+vim.opt.laststatus = 2
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.g.editorconfig = true 
+-- gui
+vim.opt.background = 'dark'
+vim.opt.termguicolors = true      --  24-bit RGB colors
+vim.cmd'colorscheme nightfly'
+
+
+-- coc
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+-- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
+-- delays and poor user experience
+vim.opt.updatetime = 300
+
+-- Always show the signcolumn, otherwise it would shift the text each time
+-- diagnostics appeared/became resolved
+vim.opt.signcolumn = "yes"
