c49d298cf4d5b0ea480c3ecae03d0077b79fc731
loki <gunlinux@ya.ru>
2024-01-31 15:15:28 +1000

    moving from coc to lsp-config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
-  { 
-	  'nvim-telescope/telescope.nvim', 
+  {
+	  'nvim-telescope/telescope.nvim',
 	  tag = '0.1.2' ,
     dependencies = "nvim-lua/plenary.nvim",
     cmd = "Telescope",
@@ -11,7 +11,23 @@ return {
             { "<Leader>fk", "<CMD>Telescope keymaps<CR>", mode = { "n", "i", "v" } },
             { "<Leader>fb", "<CMD>Telescope buffers<CR>", mode = { "n", "i", "v" } },
         },
-        config = true 
+        config = true
   },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+            -- even more opts
+          }
+        }
+      }
+    }
+    require("telescope").load_extension("ui-select")
+    end
+  }
 }
 
