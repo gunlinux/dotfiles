89ed3cccdfa78d4519eb70504b96ecdfd240ae2e
loki <gunlinux@ya.ru>
2024-02-03 06:28:40 +1000

    fixed desc for which-keys

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,11 @@
 return {
   "akinsho/bufferline.nvim",
   version = "*",
-  lazy = false,
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require("bufferline").setup()
   end,
   keys = {
-    { "<Leader>bl", "<CMD>BufferLinePick<CR>", mode = { "n", "i", "v" } },
+    { "<Leader>bl", "<CMD>BufferLinePick<CR>", mode = { "n", "i", "v" }, desc = "bufferline pick" },
   },
 }
