ec5429f93bef6de776d38c3aa44379c0903a4e68
loki <gunlinux@ya.ru>
2024-02-24 07:11:17 +1000

    broken autostart

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -40,6 +40,6 @@ vim.g.mapleader = " "
 map("n", "<Left>", ":bp<CR>", { noremap = true })
 map("n", "<Right>", ":bn<CR>", { noremap = true })
 
-vim.keymap.set({"n"}, "<F4>", ":split | terminal env python3 %<CR>", { noremap = true})
-vim.keymap.set({"n"}, "<F3>", ":bd!<CR>", { noremap = true})
+vim.keymap.set({"n"}, "<F6>", ":split | terminal env python3 %<CR>", { noremap = true})
+vim.keymap.set({"n"}, "<F7>", ":bd!<CR>", { noremap = true})
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
