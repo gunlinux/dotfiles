08b4295ea44ddbf953d88605060663c5fe8299b1
loki <gunlinux@ya.ru>
2024-03-12 12:09:14 +1000

    reinit bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,7 +1,7 @@
 return {
   "akinsho/bufferline.nvim",
   version = "*",
-  lazy = false,
+  event = "VeryLazy",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require("bufferline").setup()
