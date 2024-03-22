1ec025fe03a1ec9a0a70bbca54bd453bcfc3fe02
loki <gunlinux@ya.ru>
2024-03-22 18:32:12 +1000

    hunks nav

--- nvim/lua/plugins/gitstuff.lua
+++ nvim/lua/plugins/gitstuff.lua
@@ -7,6 +7,8 @@ return {
     end,
     keys = {
       { "<Leader>gl", "<CMD>Gitsigns preview_hunk_inline<CR>", mode = { "n" }, desc = "gitsign preview" },
+      { "<Leader>gn", "<CMD>Gitsigns next_hunk<CR>", mode = { "n" }, desc = "gitsign next_hunk" },
+      { "<Leader>gp", "<CMD>Gitsigns prev_hunk<CR>", mode = { "n" }, desc = "gitsign prev_hunk" },
     },
   },
 }
