5f05445571095b2ce8e38d790dcb95823d769965
loki <gunlinux@ya.ru>
2024-03-22 15:28:59 +1000

    preview hunks

--- nvim/lua/plugins/gitstuff.lua
+++ nvim/lua/plugins/gitstuff.lua
@@ -1,8 +1,12 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
+    lazy = false,
     config = function()
       require("gitsigns").setup()
     end,
+    keys = {
+      { "<Leader>gl", "<CMD>Gitsigns preview_hunk_inline<CR>", mode = { "n" }, desc = "gitsign preview" },
+    },
   },
 }
