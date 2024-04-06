0c399787f2653a54af9d1c1acf45762844915830
loki <gunlinux@ya.ru>
2024-04-07 00:31:54 +1000

    reflake8 support

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,12 +1,15 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.flake8.with({extra_args = {"--max-line-length","100"}}),
+        require("none-ls.diagnostics.flake8"),
       },
     })
   end,
