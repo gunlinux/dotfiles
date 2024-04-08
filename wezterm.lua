8143cd9a075bc5cbbcf81dbfc516dc32b0fdf2b8
loki <gunlinux@ya.ru>
2024-04-08 10:17:07 +1000

    fullscreen binds

--- wezterm.lua
+++ wezterm.lua
@@ -16,5 +16,15 @@ config.use_fancy_tab_bar = true
 config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = 'F12',
+    mods = '',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
 -- and finally, return the configuration to wezterm
 return config
