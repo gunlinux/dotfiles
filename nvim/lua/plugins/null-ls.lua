756baf83737468a87f4072846fbcc8bffa831fb9
loki <gunlinux@ya.ru>
2024-04-10 11:14:18 +1000

    lazy load

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     "nvimtools/none-ls-extras.nvim",
   },
+  ft = {"python", "vim", "lua"},
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
