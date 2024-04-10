a66c7108cc48ca7b26639c18ef895aed6716ec7c
loki <gunlinux@ya.ru>
2024-04-10 11:13:31 +1000

    recodium

--- nvim/lua/plugins/codium.lua
+++ nvim/lua/plugins/codium.lua
@@ -1,12 +1,24 @@
 return {
   {
-    'Exafunction/codeium.vim',
-      config = function ()
-        vim.keymap.set('i', '<F1>', function  () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-        vim.keymap.set('i', '<Leader>;', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-        vim.keymap.set('i', '<Leader>,', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-        vim.keymap.set('i', '<Leader>x', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-  end
+    verylazy = true,
+    ft = { "python", "go", "bash" },
+    "Exafunction/codeium.vim",
+    config = function()
+      vim.keymap.set("i", "<F1>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+
+      vim.keymap.set("i", "<Leader>;", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+
+      vim.keymap.set("i", "<Leader>,", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+
+      vim.keymap.set("i", "<Leader>x", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
+    end,
   },
 }
-
