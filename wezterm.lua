d6dc38a718463d992e07ad20ac6c1e0481be0272
loki <gunlinux@ya.ru>
2024-04-12 20:34:42 +1000

    windows wezterm setup

--- wezterm.lua
+++ wezterm.lua
@@ -11,10 +11,50 @@ config.color_scheme = 'Solarized Dark - Patched'
 config.audible_bell = "Disabled"
 config.use_fancy_tab_bar = true
 
-
--- config.window_background_image = "path"
 config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
+
+local dimmer = {
+  brightness = 0.03,
+  hue = 1,
+  saturation = 0.5,
+}
+
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  -- setup specific for windows
+  local wsl_domains = wezterm.default_wsl_domains()
+
+  for _, dom in ipairs(wsl_domains) do
+    if dom.name == 'WSL:Ubuntu' then
+      dom.default_prog = { '/bin/bash' }
+    end
+  end
+
+  config.wsl_domains = wsl_domains
+  config.default_domain = 'WSL:Ubuntu'
+  config.window_padding = {
+    left = '1cell',
+    right = '1cell',
+    top = '2cell',
+    bottom = '1cell',
+  }
+end
+
+if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
+  -- setup specific for linux
+end
+
+config.background = {
+  {
+    source = {
+      File = wezterm.home_dir .. '/wall.jpg'
+    },
+    repeat_x = 'Mirror',
+    hsb = dimmer,
+    attachment = { Parallax = 0.1 },
+  },
+}
 
 config.keys = {
   -- Turn off the default CMD-m Hide action, allowing CMD-m to
@@ -26,6 +66,5 @@ config.keys = {
   },
 }
 
-config.default_prog = {'bash'}
 -- and finally, return the configuration to wezterm
 return config
