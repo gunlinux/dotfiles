6defdcdbe01a3cea09a19eb0e4c3fa09e48cec57
loki <gunlinux@ya.ru>
2024-04-21 06:35:55 +1000

    basic trouble

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+      { "<Leader>xx", "<CMD>TroubleToggle<CR>",  mode = { "n", "v" }, desc = "Telescope find file" },
+  },
+ opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
