c58f5acfb743ce9dc2b1e2f110fc10ac30acff4c
loki <gunlinux@ya.ru>
2024-05-01 01:58:35 +1000

    relayout for python-dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -82,33 +82,27 @@ return {
         },
         layouts = {
           {
+            -- You can change the order of elements in the sidebar
             elements = {
+              -- Provide IDs as strings or tables with "id" and "size" keys
               {
                 id = "scopes",
-                size = 0.30,
+                size = 0.25, -- Can be float or integer > 1
               },
-              {
-                id = "breakpoints",
-                size = 0.30,
-              },
-              {
-                id = "console",
-                size = 0.30,
-              },
+              { id = "breakpoints", size = 0.25 },
+              { id = "stacks",      size = 0.25 },
+              { id = "watches",     size = 0.25 },
             },
-            position = "left",
             size = 30,
+            position = "left", -- Can be "left" or "right"
           },
           {
             elements = {
-
-              {
-                id = "repl",
-                size = 1,
-              },
+              "repl",
+              "console",
             },
-            position = "bottom",
-            size = 15,
+            size = 10,
+            position = "bottom", -- Can be "bottom" or "top"
           },
         },
         mappings = {
