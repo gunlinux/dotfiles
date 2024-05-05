e215140f4791fa6b77fcdf46b00ab14bddd23415
loki <gunlinux@ya.ru>
2024-05-05 17:22:08 +1000

    tweaked wezterm tabs

--- wezterm.lua
+++ wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'Solarized Dark - Patched'
 config.audible_bell = "Disabled"
-config.use_fancy_tab_bar = false 
+config.use_fancy_tab_bar = false
 config.exit_behavior = 'Close'
 
 
@@ -81,60 +81,30 @@ end
 -- Adopts the current default fancy styling with more padding, more distinct tab outline, glyphs/buttons
 config.colors = {
   tab_bar = {
-    -- The color of the strip that goes along the top of the window
-    -- (does not apply when fancy tab bar is in use)
     background = 'rgba(11,11,18,80%)',
-    -- The active tab is the one that has focus in the window
     active_tab = {
-      -- The color of the background area for the tab
-      --bg_color = 'rgba(0,43,54, 100%)',
-      bg_color = '#001822',
-      -- The color of the text for the tab
-      fg_color = '#c0c0c0',
-
-      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-      -- label shown for this tab.
-      -- The default is "Normal"
-      intensity = 'Normal',
-
-      underline = 'None',
-      italic = false,
-      strikethrough = false,
+      bg_color = 'rgba(0,30,39, 80%)',
+      fg_color = '#708284',
     },
 
-    -- Inactive tabs are the tabs that do not have focus
     inactive_tab = {
       bg_color = 'rgba(0,0,0,70%)',
-      fg_color = '#808080',
-      -- The same options that were listed under the `active_tab` section above
-      -- can also be used for `inactive_tab`.
+      fg_color = 'rgba(112,130,132)',
     },
 
-    -- You can configure some alternate styling when the mouse pointer
-    -- moves over inactive tabs
     inactive_tab_hover = {
-      bg_color = '#3b3052',
-      fg_color = '#909090',
-      italic = false,
-
-      -- The same options that were listed under the `active_tab` section above
-      -- can also be used for `inactive_tab_hover`.
+      bg_color = 'rgba(0,30,39, 100%)',
+      fg_color = '#708284',
     },
 
-    -- The new tab button that let you create new tabs
     new_tab = {
-      bg_color = '#4f006f',
-      fg_color = 'rgba(0,0,0,50%)',
+      bg_color = 'rgba(11,11,18,80%)',
+      fg_color = 'rgba(112, 130, 132, 100%)',
     },
 
-    -- You can configure some alternate styling when the mouse pointer
-    -- moves over the new tab button
     new_tab_hover = {
-      bg_color = '#3b3052',
-      fg_color = '#909090',
-      italic = true,
-      -- The same options that were listed under the `active_tab` section above
-      -- can also be used for `new_tab_hover`.
+      bg_color = 'rgba(0,30,39, 80%)',
+      fg_color = '#c61c6f',
     },
   },
 }
