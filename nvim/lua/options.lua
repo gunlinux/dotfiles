e8ae3f6119fa7a96366fc09df182ee8ab3f141dd
loki <gunlinux@ya.ru>
2024-05-10 19:13:33 +1000

    lower styling checks

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -35,7 +35,7 @@ vim.opt.writebackup = false
 
 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
 -- delays and poor user experience
-vim.opt.updatetime = 300
+vim.opt.updatetime = 700
 
 -- Always show the signcolumn, otherwise it would shift the text each time
 -- diagnostics appeared/became resolved
