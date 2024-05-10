95dc9785f3fe389293cf1cc0571db55491419d26
loki <gunlinux@ya.ru>
2024-05-11 04:24:18 +1000

    basic docker

--- go.sh
+++ go.sh
@@ -2,31 +2,51 @@
 
 # vim
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/swap
-mkdir -p ~/.vim/colors
-mkdir -p ~/.vim/undo
-cp vim/colors/* ~/.vim/colors/
-cp vimrc ~/.vimrc
-vim +PluginInstall +qall
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#mkdir -p ~/.vim/backup
+#mkdir -p ~/.vim/swap
+#mkdir -p ~/.vim/colors
+#mkdir -p ~/.vim/undo
+#cp vim/colors/* ~/.vim/colors/
+#cp vimrc ~/.vimrc
+##vim +PluginInstall +qall
 
 
 # bash
 
-cp bash_aliases ~/.bash_aliases
-cp bashrc ~/.bashrc
+ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
+cp -f ~/dotfiles/bashrc ~/.bashrc
 
 # git
 
-cp gitconfig ~/.gitconfig
-cp gitignore_global ~/.gitignore_global
+ln -s gitconfig ~/.gitconfig
+ln -s gitignore_global ~/.gitignore_global
 
 # stuff
-
+mkdir -p ~/.config
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-cp editorconfig ~/.editorconfig
-cp wezterm.lua ~/.wezterm.lua
+ln -s ~/dotfiles/editorconfig ~/.editorconfig
+ln -s ~/dotfiles/wezterm.lua ~/.wezterm.lua
+ln -s ~/dotfiles/nvim ~/.config/nvim
 
 # tmux tpm manage
+mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+
+if [ "$EUID" -eq 0 ] ; then
+  prefix=''
+else
+  prefix='sudo'
+fi
+
+# install deps
+if [ -f /etc/debian_version ]; then
+  $prefix apt install -y ripgrep tmux
+fi
+
+# deps for arch
+if [ -f /etc/arch-release ]; then
+  echo "i use arch btw"
+  $prefix pacman -S ripgrep eza tmux
+fi
