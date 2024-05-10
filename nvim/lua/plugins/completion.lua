81a00fe470284ae140897d152f1a504505fac2a0
loki <gunlinux@ya.ru>
2024-05-10 20:04:01 +1000

    disable annoying completition

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -3,24 +3,10 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
-
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
@@ -28,13 +14,15 @@ return {
         mapping = cmp.mapping.preset.insert({
           --["<C-b>"] = cmp.mapping.scroll_docs(-4),
           --["<C-f>"] = cmp.mapping.scroll_docs(4),
-          --["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
+	completion = {
+	  autocomplete = false
+        },
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
+          { name = "nvim_lsp" , keyword_length =2},
         }, {
           { name = "buffer" },
         }),
