834c9bfa4ea221d7a86700777843b9c27dd935cf
loki <gunlinux@ya.ru>
2024-05-21 08:29:47 +1000

    renvim

---
+++ nvim/lua/plugins/multiline.lua
@@ -0,0 +1,11 @@
+return {
+    "brenton-leighton/multiple-cursors.nvim",
+    version = "*",  -- Use the latest tagged version
+    opts = {},  -- This causes the plugin setup function to be called
+    keys = {
+      {"<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "i", "x"}, desc = "Add a cursor then move down"},
+      {"<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "i", "x"}, desc = "Add a cursor then move up"},
+      {"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}, desc = "Add or remove a cursor"},
+      {"<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to the word under the cursor"},
+  },
+}
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -6,7 +6,45 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  config = function ()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      default_component_configs = {
+        container = {
+          enable_character_fade = false,
+          modified = {
+            symbol = "✚",
+            highlight = "NeoTreeModified",
+          },
+          name = {
+            trailing_slash = false,
+            use_git_status_colors = true,
+            highlight = "NeoTreeFileName",
+          },
+
+          git_status = {
+            symbols = {
+              -- Change type
+              added     = "", 
+              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted   = "✖",-- this can only be used in the git_status source
+              renamed   = "󰁕",-- this can only be used in the git_status source
+              -- Status type
+              untracked = "",
+              ignored   = "",
+              unstaged  = "󰄱",
+              staged    = "",
+              conflict  = "",
+            }
+          },
+
+        },
+      }
+    })
+  end,
+
   cmd = "Neotree",
+
   keys = {
     { "<Leader><Tab>", "<CMD>Neotree toggle<CR>", mode = { "n", "v" } },
   },
