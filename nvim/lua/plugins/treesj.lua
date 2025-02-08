b51890d72af86864cb16d0ed98f2803a071d6c85
loki <gunlinux@ya.ru>
2025-02-09 02:31:27 +1000

    remove unused

--- nvim/lua/plugins/treesj.lua
+++
@@ -1,9 +0,0 @@
- return {
-  'Wansmer/treesj',
-  ft = { "python", "go" , "javascript" },
-  keys = { '<Leader>m', '<Leader>j', '<Leader>s' },
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  config = function()
-    require('treesj').setup({--[[ your config ]]})
-  end,
-}
