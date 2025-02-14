573e14f5dc3e827e1213a625827982124ecdb490
loki <gunlinux@ya.ru>
2025-02-14 15:27:56 +1000

    resetup markdown

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,21 +4,12 @@ return {
     "tadmccorkle/markdown.nvim",
     ft = "markdown", -- or 'event = "VeryLazy"'
     keys = {
-      { "<Leader>mo", "<CMD>MDListItemBelow<CR>", mode = { "n", "i" }, desc = "markdown list below" },
-      { "<Leader>mO", "<CMD>MDListItemAbove<CR>", mode = { "n", "i" }, desc = "markdown list below" },
-      { "<Leader>mt", "<CMD>MDTaskToggle<CR>", mode = { "n", "i" }, desc = "markdown task toggle" },
+      { "<Leader>mo", "<CMD>MDListItemBelow<CR>", mode = { "n" }, desc = "markdown list below" },
+      { "<Leader>mO", "<CMD>MDListItemAbove<CR>", mode = { "n" }, desc = "markdown list below" },
+      { "<Leader>mt", "<CMD>MDTaskToggle<CR>", mode = { "n" }, desc = "markdown task toggle" },
     },
     opts = {
       -- configuration here or empty for defaults
-    },
-  },
-  {
-    "ellisonleao/glow.nvim",
-    ft = "markdown",
-    config = true,
-    cmd = "Glow",
-    keys = {
-      { "<Leader>mp", "<CMD>Glow<CR>", mode = { "n" }, desc = "markdown preview" },
     },
   },
   {
@@ -29,7 +20,7 @@ return {
         require('render-markdown').setup({})
     end,
     keys = {
-      { "<Leader>md", "<CMD>RenderMarkdownToggle<CR>", mode = { "n" }, desc = "markdown toggle preview" },
+      { "<Leader>md", "<CMD>RenderMarkdown toggle<CR>", mode = { "n" }, desc = "markdown toggle preview" },
     },
   }
 }
