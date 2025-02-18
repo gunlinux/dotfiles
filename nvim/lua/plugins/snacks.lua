9a4855bb109c4d4b47c1c9c4ad0ab066f84409a3
loki <gunlinux@ya.ru>
2025-02-19 03:30:20 +1000

    HELLO WORLD

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+-- lazy.nvim
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}
