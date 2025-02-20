c1f44b223527baeb5730421a20644ca7cfde6ef7
loki <gunlinux@ya.ru>
2025-02-21 03:51:00 +1000

    lazydocker

---
+++ nvim/lua/plugins/lazydocker.lua
@@ -0,0 +1,19 @@
+return {
+  "mgierada/lazydocker.nvim",
+  dependencies = { "akinsho/toggleterm.nvim" },
+  config = function()
+    require("lazydocker").setup({
+	    border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+    })
+  end,
+  event = "BufRead",
+  keys = {
+    {
+      "<leader>ld",
+      function()
+        require("lazydocker").open()
+      end,
+      desc = "Open Lazydocker floating window",
+    },
+  },
+}
