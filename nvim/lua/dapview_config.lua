46853f4e16bdaf593b71ee64544bff9b7a8a28fe
loki <gunlinux@ya.ru>
2025-02-22 18:42:27 +1000

    redap ui

---
+++ nvim/lua/dapview_config.lua
@@ -0,0 +1,10 @@
+return {
+    windows = {
+        terminal = {
+            -- NOTE Don't copy paste this snippet
+            -- Use the actual names for the adapters you want to hide
+            -- `delve` is known to not use the terminal
+            hide = { "debugpy"},
+        },
+    },
+}
--- nvim/lua/plugins/dap_ui.lua
+++ nvim/lua/plugins/dap_ui.lua
@@ -1,93 +1,94 @@
 return {
-  {
-    event = "VeryLazy",
+  event = "VeryLazy",
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "nvim-neotest/nvim-nio",
     "jay-babu/mason-nvim-dap.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      local python_path = table.concat(
-      { vim.fn.stdpath('data'), 'mason', 'packages', 'debugpy', 'venv', 'bin', 'python' }, '/'):gsub('//+', '/')
-      require('mason-nvim-dap').setup({
-        ensure_installed = { "python", "delve" },
-        handlers = {
-          function(config)
-            -- Keep original functionality
-            require('mason-nvim-dap').default_setup(config)
-          end,
-
-          python = function(config)
-            config.adapters = {
-              type = "executable",
-              command = python_path,
-              args = {
-                "-m",
-                "debugpy.adapter",
-              },
-            }
-            require('mason-nvim-dap').default_setup(config)   -- don't forget this!
-          end,
-        },
-      })
-    end,
+    { "igorlfs/nvim-dap-view", opts = require('dapview_config') },
   },
-  {
-    event = "VeryLazy",
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio",
-      "jay-babu/mason-nvim-dap.nvim",
+  config = function()
+    local python_path = table.concat(
+    { vim.fn.stdpath('data'), 'mason', 'packages', 'debugpy', 'venv', 'bin', 'python' }, '/'):gsub('//+', '/')
+    require('mason-nvim-dap').setup({
+      ensure_installed = { "python", "delve" },
+      handlers = {
+        function(config)
+          -- Keep original functionality
+          require('mason-nvim-dap').default_setup(config)
+        end,
+
+        python = function(config)
+          config.adapters = {
+            type = "executable",
+            command = python_path,
+            args = {
+              "-m",
+              "debugpy.adapter",
+            },
+          }
+          require('mason-nvim-dap').default_setup(config)   -- don't forget this!
+        end,
+      },
+    })
+    --require("dapui").setup()
+    local dap, dapui = require("dap"), require("dap-view")
+    dap.defaults.fallback.terminal_win_cmd = nil
+    dap.listeners.before.attach.dapui_config = function()
+      --daopui.open()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      --dapui.open()
+      dapui.close()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      --dapui.close()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      --dapui.close()
+      --
+      dapui.close()
+    end
+  end,
+  -- dap binds
+  keys = {
+     {
+      "<Leader>d",
+      "<CMD>DapViewToggle<CR>",
+      mode = { "n", "v" },
+      desc = "Toggle Breakpoint"
     },
-    config = function()
-      require("dapui").setup()
-      local dap, dapui = require("dap"), require("dapui")
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    end,
-    -- dap binds
-    keys = {
-      {
-        "<Leader><F8>",
-        "<CMD>DapToggleBreakpoint<CR>",
-        mode = { "n", "v" },
-        desc = "Toggle Breakpoint"
-      },
-      {
-        "<F5>",
-        "<CMD>DapContinue<CR>",
-        mode = { "n", "v" },
-        desc = "Dap Continue"
-      },
-      {
-        "<F7>",
-        "<CMD>DapStepInto<CR>",
-        mode = { "n", "v" },
-        desc = "Dap Continue"
-      },
-      {
-        "<F8>",
-        "<CMD>DapStepInto<CR>",
-        mode = { "n", "v" },
-        desc = "Dap Continue"
-      },
-      {
-        "<F9>",
-        "<CMD>DapStepOver<CR>",
-        mode = { "n", "v" },
-        desc = "Dap Continue"
-      },
+    {
+      "<Leader><F8>",
+      "<CMD>DapToggleBreakpoint<CR>",
+      mode = { "n", "v" },
+      desc = "Toggle Breakpoint"
     },
-  }
+    {
+      "<F5>",
+      "<CMD>DapContinue<CR>",
+      mode = { "n", "v" },
+      desc = "Dap Continue"
+    },
+    {
+      "<F7>",
+      "<CMD>DapStepInto<CR>",
+      mode = { "n", "v" },
+      desc = "Dap Continue"
+    },
+    {
+      "<F8>",
+      "<CMD>DapStepInto<CR>",
+      mode = { "n", "v" },
+      desc = "Dap Continue"
+    },
+    {
+      "<F9>",
+      "<CMD>DapStepOver<CR>",
+      mode = { "n", "v" },
+      desc = "Dap Continue"
+    },
+  },
 }
