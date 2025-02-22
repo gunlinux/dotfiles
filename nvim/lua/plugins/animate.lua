394bc2db46a01b290693edc0517ef1d6e99c7000
loki <gunlinux@ya.ru>
2025-02-22 20:34:58 +1000

    disable buggy animation

--- nvim/lua/plugins/animate.lua
+++ nvim/lua/plugins/animate.lua
@@ -5,6 +5,11 @@ return {
     "echasnovski/mini.icons",
   },
   config = function()
-    require('mini.animate').setup()
+    require('mini.animate').setup({
+      scroll = {
+        -- Whether to enable this animation
+        enable = false,
+      },
+    })
   end,
 }
