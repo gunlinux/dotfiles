79b57f8fc4598c33259d82f036a9687cece527ef
loki <gunlinux@ya.ru>
2025-06-07 03:17:51 +1000

    style

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,6 +23,13 @@ return {
     config = function()
       local telescope = require("telescope")
       telescope.setup({
+        defaults = {
+            layout_strategy = "vertical",
+            layout_config = { height=1, width=1 },
+        },
+        vertical = {
+          width = 0.3
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({
