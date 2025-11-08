4312bbc41b5e20bb36af7a3106d6d9dcd530bf18
loki <gunlinux@ya.ru>
2025-11-08 22:30:32 +1000

    recolor

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,28 +1,16 @@
 return {
-  "gunlinux/solarized-osaka.nvim",
+  "craftzdog/solarized-osaka.nvim",
   lazy = false,
   priority = 1000,
-  opts = {
-    styles = {
-      sidebars = "transparent",
-    }
-  }
-  ,
   config = function()
     require("solarized-osaka").setup({
-      sidebars = { "qf", "vista_kind", "terminal", "packer" },
-      style = "dark", -- "dark" or "light"
-      transparent = true,
+      sidebars = { "vista_kind", "terminal", "packer" },
       styles = {
         sidebars = "transparent",
-      }
+      },
+      style = "dark",
+      transparent = true,
     })
-    --[[
-    require('mini.animate').setup({
-      open = { enable = false },
-      close = { enable = false },
-    })
-    ]] --
     vim.cmd [[colorscheme solarized-osaka]]
   end,
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,14 +3,10 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    "gunlinux/solarized-osaka.nvim",
   },
   config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        theme = 'solarized-osaka'
-      },
-    })
-  end,
+    require('lualine').setup {
+      options = { theme = 'solarized_dark' },
+    }
+  end
 }
